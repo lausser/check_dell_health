119492fbc90d23e89448cdee36993fa76cfdfa78
Gerhard Lausser <gerhard.lausser@consol.de>
2017-11-24 00:05:37 +0100

    output idrac type+firmware and server model

--- plugins-scripts/Classes/Dell/IDRAC.pm
+++ plugins-scripts/Classes/Dell/IDRAC.pm
@@ -9,8 +9,17 @@ sub init {
 #    if ($self->implements_mib('DELL-RAC-MIB')) {
 #      $self->analyze_and_check_environmental_subsystem('Classes::Dell::RAC::Components::EnvironmentalSubsystem');
 #    }
+  } elsif ($self->mode =~ /device::uptime/) {
+    $self->SUPER::init();
   } else {
     $self->no_such_mode();
   }
 }
 
+sub pretty_sysdesc {
+  my $self = shift;
+  $self->get_snmp_objects('IDRAC-MIB-SMIv2', qw(racShortName
+      racFirmwareVersion systemModelName));
+  return sprintf "%s@%s (FW: %s)", $self->{racShortName},
+      $self->{systemModelName}, $self->{racFirmwareVersion};
+}
